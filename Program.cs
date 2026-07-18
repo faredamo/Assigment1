422a9ca316e0d5235b76f1a75c970386b9f80ced
faredamo <faridamohamed229419@gmail.com>
2026-07-18 23:21:14 +0300

    Solve Task 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿#region Task 1
+using System;
+class Book
+{
+    public string Title;
+    public int Pages;
+    public override string ToString()
+    {
+        return $"Title:{Title}, Pages:{Pages}";
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        object myBook = new Book { Title = "WORLD WAR||", Pages = 400 };
+
+        Console.WriteLine(myBook);
+    }
+}
+#endregion
